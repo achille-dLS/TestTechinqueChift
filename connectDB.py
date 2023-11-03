2d3d66e29852cbc80ef573c78a3b133e9df35b0d
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2023-11-03 15:21:38 +0100

    creation objet employe

--- connectDB.py
+++ connectDB.py
@@ -1,4 +1,15 @@
 import psycopg2
+
+
+
+class Employe:
+    def __init__(self,id,nom,num_pro,email,departement):
+        self.id = id
+        self.nom = nom
+        self.num_pro = num_pro
+        self.email = email
+        self.departement = departement
+
 try:
     # connection a la db
     conn = psycopg2.connect(
@@ -19,6 +30,17 @@ try:
     employees = cursor.fetchall()
     # print answer
     print(employees)
+
+    # get one employees
+    print('get 1st employe name')
+    # the request to execute
+    cursor.execute('SELECT * FROM employees.employe WHERE id = 1')
+    # getting the answer to the request
+    rs = cursor.fetchone()
+    employe1 = Employe(rs[0],rs[1],rs[2],rs[3],rs[4])
+    # print answer
+
+    print(employe1.nom)
 
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
