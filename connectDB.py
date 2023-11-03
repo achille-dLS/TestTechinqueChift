16aaddc681dad45d23b0532e63fba1f9fc3e225d
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2023-11-03 15:12:47 +0100

    connection et récup 1er objets de la db

--- connectDB.py
+++ connectDB.py
@@ -1,8 +1,27 @@
 import psycopg2
+try:
+    # connection a la db
+    conn = psycopg2.connect(
+        host="localhost",
+        database="postgres",
+        user="postgres",
+        password="Achille.500"
+        )
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="postgres",
-    user="postgres",
-    password="Achille.500")
+
+    cursor = conn.cursor()
+
+    # get all employees
+    print('get all employees')
+    # the request to execute
+    cursor.execute('SELECT * FROM employees.employe')
+    # getting the answer to the request
+    employees = cursor.fetchall()
+    # print answer
+    print(employees)
+
+except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+
+conn.close()
 
