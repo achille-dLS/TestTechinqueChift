fd69f15beb2fc2c7b9adbf62d76b7a6cb6fd731c
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2023-11-05 15:12:55 +0100

    modif nom connect db to APIEmployees
    modification du nom du fichier + ajout connection db a chaque requete

---
+++ APIEmployees.py
@@ -0,0 +1,74 @@
+import psycopg2
+from fastapi import FastAPI
+
+
+
+class Employe:
+    def __init__(self,id,nom,num_pro,email,departement):
+        self.id = id
+        self.nom = nom
+        self.num_pro = num_pro
+        self.email = email
+        self.departement = departement
+
+app = FastAPI()
+
+## REQUETES API
+@app.get('/')
+async def root():
+    return {'API working' : 'hey there buddy'}
+
+@app.get('/employees/')
+async def getAllEmployees():
+
+    try:
+        # connection a la db
+        conn = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user="postgres",
+            password="Achille.500"
+            )
+        cursor = conn.cursor()
+        # request to execute
+        cursor.execute('SELECT * FROM employees.employe')
+        # getting the answer to the request
+        employees = cursor.fetchall()
+        return employees
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+@app.get('/employees/{id}')
+async def getEmployeById(id:int):
+    try:
+        # connection a la db
+        conn = psycopg2.connect(
+            host="localhost",
+            database="postgres",
+            user="postgres",
+            password="Achille.500"
+            )
+        cursor = conn.cursor()
+
+        
+        # the request to execute
+        querry = 'SELECT * FROM employees.employe WHERE id = ' + str(id)
+        cursor.execute(querry)
+        # getting the answer to the request
+        rs = cursor.fetchone()
+        employe1 = Employe(rs[0],rs[1],rs[2],rs[3],rs[4])
+        return employe1
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+
+
+    
+
+
+
+
+
+
